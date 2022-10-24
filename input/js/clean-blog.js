5dafe369671706ff5b5d3fcab3be4fb2e30b825f
Riccardo De Agostini <me@riccar.do>
2022-10-24 13:36:10 +0200

    Remove styles and scripts that were only needed by the "Contact Me" page

--- input/js/clean-blog.js
+++ input/js/clean-blog.js
@@ -1,14 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
-  });
 
   // Show the navbar when the page is scrolled up
   var MQL = 992;
